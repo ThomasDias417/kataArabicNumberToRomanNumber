1d60e289e383736d6865b885ac5770be425989c5
thomas <thomasdias417@gmail.com>
2023-11-07 16:46:33 +0100

    feature 1 to 10

---
+++ __test__/first.test.js
@@ -0,0 +1,26 @@
+
+const {convertArabicNumberToRomanNumber} = require('../kataArabicToRomanNumber.js')
+
+test('convert 1 to I',()=>{
+    expect(convertArabicNumberToRomanNumber(1)).toEqual('I')
+})
+
+test('convert 3 to III',()=>{
+    expect(convertArabicNumberToRomanNumber(3)).toEqual('III')
+})
+
+test('convert 5 to V',()=>{
+    expect(convertArabicNumberToRomanNumber(5)).toEqual('V')
+})
+
+test('convert 7 to VII',()=>{
+    expect(convertArabicNumberToRomanNumber(7)).toEqual('VII')
+})
+
+test('convert 5 to V',()=>{
+    expect(convertArabicNumberToRomanNumber(5)).toEqual('V')
+})
+
+test('cumul opérande',()=>{
+    expect(convertArabicNumberToRomanNumber(12).toEqual('XII'))
+})
--- kataArabicToRomanNumber.js
+++ kataArabicToRomanNumber.js
@@ -1,3 +1,10 @@
-function convertArabicNumberToRomanNumber(){
+ function convertArabicNumberToRomanNumber(number){
+    const equivalence = ['I','II','III','IV','V','VI','VII','VIII','IX','X']
     
+    const rank = ['','L','C','D','M']
+    return equivalence[number-1]
+}
+
+module.exports = {
+    convertArabicNumberToRomanNumber
 }
