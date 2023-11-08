a45167abd312daaa3911bd67531cab4b54036fc9
thomas <thomasdias417@gmail.com>
2023-11-08 17:04:59 +0100

    fix route api

--- api/routes/converterAtoR.js
+++ api/routes/converterAtoR.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const convertAtoRroutes = express.Router()
-const {getConversion,getTestBundle} = require('../controler/convertAtoR')
+const {getConversionAtoR,getTestBundle} = require('../controler/convertAtoR')
+const {getConversionRtoA} = require('../controler/convertRtoA')
 
 convertAtoRroutes.get('/',async(req,res,next)=>{
-     getConversion(req,res)
+    getConversionAtoR(req,res)
 })
+
+convertAtoRroutes.get('/reverse',async(req,res,next)=>{
+    getConversionRtoA(req,res)
+})
+
 
 convertAtoRroutes.get('/testBundle',async(req,res,next)=>{
     getTestBundle(req,res)
